87cebcf64b84a709e89579d3b22f5915c1ee6095
瑜笙 <2624367622@qq.com>
2024-02-21 08:38:28 +0800

    细节优化

--- my-docs/src/.vuepress/theme.ts
+++ my-docs/src/.vuepress/theme.ts
@@ -61,10 +61,10 @@ export default hopeTheme({
 
     // 在启用之前需要安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务器，在生产环境中请自行部署并使用自己的服务器！
-    // comment: {
-    //   provider: "Waline",
-    //   serverURL: "https://waline-comment.vuejs.press",
-    // },
+     comment: {
+       provider: "Waline",
+       serverURL: "https://pinglun.botqsign.icu",
+     },
 
     components: {
       components: ["Badge", "VPCard"],
