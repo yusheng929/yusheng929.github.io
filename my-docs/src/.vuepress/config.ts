4af6747c69328292113bce0bfd88501fdeef330e
theqingyao <2624367622@qq.com>
2024-03-15 15:16:16 +0800

    细节优化

--- my-docs/src/.vuepress/config.ts
+++ my-docs/src/.vuepress/config.ts
@@ -28,20 +28,20 @@ export default defineUserConfig({
     text: "首页",
     icon: "laptop-code",
     link: "/",
-    activeMatch: "^/"
+    activeMatch: "^/",
   },
   {
     text: "公共QSign文档",
     icon: "laptop-code",
     link: "/qsign.md",
-    activeMatch: "^/qsign"
+    activeMatch: "^/qsign",
   },
   {
     text: "联系方式",
     icon: "laptop-code",
     link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nmgX9UiuKJD1J_jZBmH7VftZ69iBkh5i&authKey=ElhhVv0EjlO2cj%2Bpys%2BymALb1xSeyl95G79hVT6GIgd8asQVnoGiuDXlcdAsXAEj&noverify=0&group_code=595480404",
   },
-  ]
+  ],
 
   // 页脚
   footer: "默认页脚",
