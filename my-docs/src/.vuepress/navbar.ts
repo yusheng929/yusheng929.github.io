550caf9ff8f9ebe035e9229a47f5c5f924af40b6
theqingyao <2624367622@qq.com>
2024-03-15 14:32:19 +0800

    细节优化

---
+++ my-docs/src/.vuepress/navbar.ts
@@ -0,0 +1,21 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  {
+    text: "首页",
+    icon: "laptop-code",
+    prefix: "./README.md",
+    link: "/",
+  },
+  {
+    text: "公共QSign文档",
+    icon: "laptop-code",
+    prefix: "./qsign.md",
+    link: "/qsign",
+  },
+  {
+    text: "联系方式",
+    icon: "laptop-code",
+    link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nmgX9UiuKJD1J_jZBmH7VftZ69iBkh5i&authKey=ElhhVv0EjlO2cj%2Bpys%2BymALb1xSeyl95G79hVT6GIgd8asQVnoGiuDXlcdAsXAEj&noverify=0&group_code=595480404",
+  },
+]);
---
+++ my-docs/src/.vuepress/sidebar.ts
@@ -0,0 +1,25 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+    {
+      text: "首页",
+      icon: "laptop-code",
+      prefix: "README.md",
+      link: "/",
+      children: "structure",
+    },
+    {
+      text: "公共QSign文档",
+      icon: "laptop-code",
+      prefix: "./qsign.md",
+      link: "/qsign",
+      children: "structure",
+    },
+    {
+    text: "联系方式",
+    icon: "laptop-code",
+    link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nmgX9UiuKJD1J_jZBmH7VftZ69iBkh5i&authKey=ElhhVv0EjlO2cj%2Bpys%2BymALb1xSeyl95G79hVT6GIgd8asQVnoGiuDXlcdAsXAEj&noverify=0&group_code=595480404",
+  },
+  ],
+});
